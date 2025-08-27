e2107f5e34abdb18a0aa97ee8d1ec29977c78dbf
Dominic-babson2025 <dcrisafulli1@babson.edu>
2025-08-27 14:27:12 +0000

    update

--- classes/week00/first_class/second_class_exercises.py
+++ classes/week00/first_class/second_class_exercises.py
@@ -1,13 +1,19 @@
 #%%
 # print 'hello' 5 times using an arithmetic operator
-
+print('hello' * 5)
 
 #%%
 # print 'hello' 5 times using a loop
-
+i = 0 
+for i in range(5):
+    print('hello')
 #%%
 # print 'hello' 5 times on the same line using a loop
-
+word = ''
+for i in range(5):
+    word += 'hello'
+    i += 1
+print (word)
 #%%
 ''' using nested loops print the following:
 
@@ -16,19 +22,30 @@
 20 21 22
 
 '''
+i = 0
+for i in range(3):
+    for j in range(3):
+      
+    
+
 #%%
 # define txt and input some text from the keyboard into it
-
+txt = 'my name is Dom'
 
 #%%
 # print each letter in txt 
-
+txt = 'my name is Dom'
+for i in range(len(txt)):
+    print(txt[i])
 #%%
 # assign the variable letter to the first letter in txt
-
+letter = txt[0]
 #%%
 # print out all the letters in txt that are equal to the first letter
-
+for i in range(len(txt)):
+    new_letter = txt[i]
+    if new_letter == letter:
+        print(new_letter)
 '''
 say txt = 'the cat in the hat was read today'
 't' is the first letter
@@ -55,4 +72,8 @@ result: tttt
 
 
                                     # %%
-                                    
+names_list = ["Tommy", "Dom", "Ambre", "Tom", "Phil"]
+shifted_names = []
+for i in range(len(names_list)):
+    if (i % 3) == 
+    
